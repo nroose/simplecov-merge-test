ba092d81c1280b1237d490c17b4399158c67879b
nroose <nroose@gmail.com>
2018-04-29 15:03:57 -0700

    No conditional for coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
-SimpleCov.start if ENV["COVERAGE"]
+SimpleCov.start
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
